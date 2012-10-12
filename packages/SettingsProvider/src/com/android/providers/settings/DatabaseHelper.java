ef9aaf5cf4e141b9c509fad5ef9f9a9d36808d5e
dhacker29 <davidhackerdvm@gmail.com>
2012-10-11 23:43:09 -0500

    Settings for MotoOEM Telephony and ImsiFix

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -42,6 +42,7 @@ import com.android.internal.telephony.BaseCommands;
 import com.android.internal.telephony.Phone;
 import com.android.internal.telephony.PhoneConstants;
 import com.android.internal.telephony.RILConstants;
+import com.android.internal.telephony.TelephonyProperties;
 import com.android.internal.util.XmlUtils;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.internal.widget.LockPatternView;
@@ -1502,6 +1503,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
             // Set default tty mode
             loadSetting(stmt, Settings.System.TTY_MODE, 0);
+
+            // Set the value of MOTO OEM for toggle use
+            loadSetting(stmt, Settings.System.MOTO_OEM_STATE,
+                "true".equalsIgnoreCase(SystemProperties.get(TelephonyProperties.PROPERTY_MOTO_OEM, "true")) ? "1" : "0");
+
+            // Set the value of IMSI FIX for toggle use
+            loadSetting(stmt, Settings.System.IMSI_FIX_STATE,
+                "true".equalsIgnoreCase(SystemProperties.get(TelephonyProperties.PROPERTY_IMSI_FIX, "true")) ? "1" : "0");
 
             loadBooleanSetting(stmt, Settings.System.AIRPLANE_MODE_ON,
                     R.bool.def_airplane_mode_on);
